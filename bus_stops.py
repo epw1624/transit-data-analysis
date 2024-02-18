2cb318eaeed8ce3218f4a3949bbf59fb148ab2af
Emily Wood <emily.wood1624@gmail.com>
2024-02-18 15:30:21 -0800

    filter out non bus stops

--- bus_stops.py
+++ bus_stops.py
@@ -1,9 +1,11 @@
 import pandas
 
 class BusStops:
-    COLS_TO_DROP = ["stop_url", "parent_station", "wheelchair_boarding", "stop_desc"]
+    COLS_TO_DROP = ["stop_url", "parent_station", "wheelchair_boarding", "stop_desc", "location_type"]
     def __init__(self, filepath):
         self.stops = pandas.read_csv(filepath)
         self.stops["stop_id"] = self.stops["stop_id"].astype(int)
         self.stops["location_type"] = self.stops["location_type"].astype(int)
+        self.stops = self.stops[self.stops["location_type"] == 0]
         self.stops.drop(columns=self.COLS_TO_DROP, inplace=True)
+        

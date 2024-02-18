078ebad08b1d8aa55b390bcb43c501f2c34e6d90
Emily Wood <emily.wood1624@gmail.com>
2024-02-18 14:23:21 -0800

    wrapper class for bus stops dataframe

--- bus_stops.py
+++ bus_stops.py
@@ -1,5 +1,9 @@
 import pandas
 
 class BusStops:
+    COLS_TO_DROP = ["stop_url", "parent_station", "wheelchair_boarding", "stop_desc"]
     def __init__(self, filepath):
         self.stops = pandas.read_csv(filepath)
+        self.stops["stop_id"] = self.stops["stop_id"].astype(int)
+        self.stops["location_type"] = self.stops["location_type"].astype(int)
+        self.stops.drop(columns=self.COLS_TO_DROP, inplace=True)
